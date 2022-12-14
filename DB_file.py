4a41519582bb11935f7e80cd30f2dc97c89f4d07
Aadyant <aadyant2017@gmail.com>
2022-12-14 15:37:40 +0530

    Updated DB_file with minor changes | added Error handling

--- DB_file.py
+++ DB_file.py
@@ -1,11 +1,17 @@
 import sqlite3
+from sqlite3 import Error
+import os
+
 
 def create_db():
+    # print(os.getcwd(), 'hi')
+    # Setting the location where our DataBase 'test.db' would reside.
+    os.chdir('/Users/aadyantsrivastav/Desktop/Education/pythonProject/')
     conn = sqlite3.connect('test.db')
-    c = conn.cursor()
     create_table(conn)
 
     return conn
+
 
 '''
 We are creating 3 tables 
@@ -13,23 +19,29 @@ Table-1: file_paths ---> [To maintain  - File_Name, File_Extension & File_Path]
 Table-2: word_paths ---> [To maintain  - Word & File_Path]
 Table-3: file_time_stamps ---> [To maintain  - File_Name, File_Path & TimeStamp]
 
+INDEXING
 We are indexing on the file name in file_paths and words in word_paths.
 Internally indexing will create b-trees. 
 The main purpose of indexing is to reduce the search time to O(logN), where N is 
 the number of records in the table.
+PRIMARY KEY
+We create Primary Key in our Table, just to avoid duplication of rows. 
 '''
+
 
 # CREATING TABLES
 def create_table(conn):
     c = conn.cursor()
     c.execute('''CREATE TABLE IF NOT EXISTS file_paths(
     file_name text,
-    file_path text
+    file_path text,
+    PRIMARY KEY(file_name, file_path)
     )''')
 
     c.execute('''CREATE TABLE IF NOT EXISTS word_paths(
-    word text,
-    file_path text
+    words text NOT NULL,
+    file_path text,
+    PRIMARY KEY(words, file_path)
     )''')
 
     c.execute('''CREATE TABLE IF NOT EXISTS file_time_stamps_table(
@@ -40,11 +52,12 @@ def create_table(conn):
 
     create_index(conn)
 
+
 # CREATING INDEX ON THE TABLES FOR FASTER(O(LOGn)) SEARCH
 def create_index(conn):
     c = conn.cursor()
     file_paths_table_index = "CREATE INDEX IF NOT EXISTS file_paths_index ON file_paths(file_name)"
-    word_paths_table_index = "CREATE INDEX IF NOT EXISTS word_paths_index ON word_paths(word)"
+    word_paths_table_index = "CREATE INDEX IF NOT EXISTS word_paths_index ON word_paths(words)"
 
     c.execute(file_paths_table_index)
     c.execute(word_paths_table_index)
@@ -52,29 +65,37 @@ def create_index(conn):
 
 # INSERTION INTO TABLES
 def insert_into_file_paths_table(conn, values):
-    c = conn.cursor()
-    sql = "INSERT INTO file_paths VALUES (?,?)"
-    c.execute(sql, values)
-    conn.commit()
-    print('done')
+    try:
+        c = conn.cursor()
+        sql = "INSERT INTO file_paths VALUES (?,?)"
+        c.execute(sql, values)
+        conn.commit()
+    except Error as e:
+        return
 
 def insert_into_word_paths_table(conn, values):
-    c = conn.cursor()
-    sql = "INSERT INTO word_paths VALUES (?,?)"
-    c.execute(sql, values)
-    conn.commit()
+    try:
+        c = conn.cursor()
+        sql = "INSERT INTO word_paths VALUES (?,?)"
+        c.execute(sql, values)
+        conn.commit()
+    except Error as e:
+        # print(e)
+        return
 
 def insert_into_file_time_stamps_table(conn, values):
-    c = conn.cursor()
-    sql = "INSERT INTO file_time_stamps_table VALUES (?,?,?)"
-    c.execute(sql, values)
-    conn.commit()
+    try:
+        c = conn.cursor()
+        sql = "INSERT INTO file_time_stamps_table VALUES (?,?,?)"
+        c.execute(sql, values)
+        conn.commit()
+    except Error as e:
+        return
 
 
 
 def query(conn):
     c = conn.cursor()
-
     # # GETTING LIST OF TABLES IN DB
     # c.execute('''SELECT name FROM sqlite_master
     # WHERE type='table' ''')
@@ -93,14 +114,14 @@ def query(conn):
     count = 0
     c.execute('''SELECT * FROM file_paths''')
     for i in c.fetchall():
-        print(i)
+        # print(i)
         count += 1
-    print(count)
+    print('Number of rows in file_paths table', count)
 
-
-c = create_db()
-
-
-
-
-
+    # PRINT word_path TABLE
+    count = 0
+    c.execute('''SELECT * FROM word_paths''')
+    for i in c.fetchall():
+        # print(i)
+        count += 1
+    print('Number of rows in word_paths table', count)
